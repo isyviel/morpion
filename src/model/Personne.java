c152db95a820160dcf284ce5eafd6e5f97af5f7e
tomi <kara_hakima@yahoo.fr>
2019-11-27 10:22:33 +0100

    Ajout IA & modif class personne et

--- src/model/Personne.java
+++ src/model/Personne.java
@@ -1,5 +1,16 @@
 package model;
 
-public class Personne {
+public  class Personne {
+
+    protected char msymboler;// l'attribut
+
+//methode abstracte
+public  jouer () {
+
+}
+//constructeur
+protected joueur ()
+
+
 
 }
--- src/model/Plateau.java
+++ src/model/Plateau.java
@@ -2,4 +2,13 @@ package model;
 
 public class Plateau {
 
+
+    private int mnombre_ligne;
+private int mnombre_ colomne;
+
+
+
+public Plateau (p Mnombre_ligne :int, p mnombre_colonne :string)
+
+
 }
